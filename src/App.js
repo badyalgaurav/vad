77cdc52c6bdd2d0732f1bec8373a64a07b325fff
Garry <badyalgaurav0@gmail.com>
2023-08-10 11:57:19 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import React, { useState } from "react"
 // import { interpolateInferno } from "https://cdn.skypack.dev/d3-scale-chromatic@3"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone,faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
 const App=()=>{
-    async function test(){
+  const [color,setColor]=useState("grey");
+  const [isMuted, setIsMuted] = useState(true);
+        // Apply the calculated color to the component's style
+  const componentStyle = {
+    // backgroundColor: color,
+    fontSize: '50px', // Set the icon's font size
+    color:color,
+    
+  };
+   // Apply the calculated color to the larger button's style
+   const buttonStyle = {
+    backgroundColor: "blue",
+    padding: '15px 30px', // Larger padding for a bigger button
+    color: 'white',
+    border: 'none',
+    borderRadius: '10px', // Rounded corners for a more prominent look
+    fontSize: '20px', // Larger font size
+    cursor: 'pointer',
+    display: 'block',
+
+
+  };
+
+  // Center the button and div using flexbox
+  const containerStyle = {
+    display: 'flex',
+    // flexWrap: 'wrap', // Wrap content into new lines
+    flexDirection: 'column', // Arrange items in a column
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Make the container fill the screen vertically
+    
+  };
+
+  const itemStyle = {
+    margin: '10px 0', // Add margin to create space between items
+  };
+  const indicatorStyle = {
+    color: 'red',
+  };
+
+    async function initVAD(){
 //declaring VAD
 try {
     const myvad = await window.vad.MicVAD.new({
@@ -9,8 +53,9 @@ try {
       minSpeechFrames: 5,
       preSpeechPadFrames: 10,
       onFrameProcessed: (probs) => {
-        // const indicatorColor = interpolateInferno(probs.isSpeech / 2)
-        // document.body.style.setProperty("--indicator-color", indicatorColor)
+       if (probs.notSpeech < 0.1){
+        setColor("green");
+       }
       },
       onSpeechEnd: (arr) => {
         const wavBuffer = window.vad.utils.encodeWAV(arr)
@@ -19,18 +64,22 @@ try {
         const el = addAudio(url)
         const speechList = document.getElementById("playlist")
         speechList.prepend(el)
+        setColor("grey");
+
       },
     })
     // window.myvad = myvad
 
     clearInterval(loading)
     window.toggleVAD = () => {
+    
       console.log("ran toggle vad")
       if (myvad.listening === false) {
         myvad.start()
         document.getElementById("toggle_vad_button").textContent =
           "STOP VAD"
         document.getElementById("indicator").textContent = "VAD is running"
+
       } else {
         myvad.pause()
         document.getElementById("toggle_vad_button").textContent =
@@ -38,14 +87,12 @@ try {
         document.getElementById(
           "indicator"
         ).innerHTML = `VAD is <span style="color:red">stopped</span>`
-        // const indicatorColor = interpolateInferno(0)
-        // document.body.style.setProperty("--indicator-color", indicatorColor)
       }
     }
     window.toggleVAD()
     document.getElementById("toggle_vad_button").disabled = false
     document.getElementById("toggle_vad_button").addEventListener("click",window.toggleVAD); 
-    document.getElementById("indicator").textContent = "VAD is ready. Click on start VAD"
+    // document.getElementById("indicator").textContent = "VAD is ready. Click on start VAD"
   } catch (e) {
     console.error("Failed:", e)
     clearInterval(loading)
@@ -55,7 +102,7 @@ try {
   }
   
 }
-test();
+initVAD();
 function addAudio(audioUrl) {
     const entry = document.createElement("li")
     const audio = document.createElement("audio")
@@ -71,17 +118,22 @@ function addAudio(audioUrl) {
     const [message, ...dots] = indicator.innerHTML.split(".")
     indicator.innerHTML = message + ".".repeat((dots.length + 1) % 7)
   }, 200)
+ 
+return(<>
 
-return(<><p>Hello world</p>
-<div class="control-row">
-          <div id="indicator">
-            VAD is <span style={{color: "red"}}>LOADING</span>
-          </div>
-          <button id="toggle_vad_button"  disabled>
-            START VAD
-          </button>
-        </div>
-        <ol id="playlist" reversed></ol>
+    
+    <div style={containerStyle}>
+      <div id="indicator" style={itemStyle}>
+        VAD is <span style={indicatorStyle}>LOADING</span>
+      </div>
+      <FontAwesomeIcon icon={faMicrophone} style={{ ...componentStyle, ...itemStyle }} />
+
+      <button id="toggle_vad_button" style={{ ...buttonStyle, ...itemStyle }} disabled>
+        START VAD
+      </button>
+      <ol id="playlist" reversed style={itemStyle}></ol>
+    </div>
+             
 </>)
 }
 
